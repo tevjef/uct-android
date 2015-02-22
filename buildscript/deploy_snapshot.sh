f43fe92cbd011f9ff82bd64e84f3815043eafa0f
tevjef <mastermindtj94@gmail.com>
2015-02-22 12:44:26 -0500

    more travis tweaks
    Former-commit-id: 4021c6b4a940723cbb3d65108d4faec44781150a

---
+++ buildscript/deploy_snapshot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Deploy a jar, source jar, and javadoc jar to Sonatype's snapshot repo.
+#
+# Adapted from https://coderwall.com/p/9b_lfq and
+# http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+
+SLUG="tevjef/Rutgers-Course-Tracker"
+JDK="oraclejdk7"
+BRANCH="master"
+
+set -e
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping build: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  echo "Skipping build: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping build: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping build: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+else
+  echo "building..."
+  ./gradlew build
+  echo "built!"
+fi
---
+++ gradle/start-emulator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+echo no | android create avd --force -n test -t android-18 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+curl http://is.gd/android_wait_for_emulator > android-wait-for-emulator
+chmod u+x android-wait-for-emulator
+./android-wait-for-emulator
+adb shell input keyevent 82 &
+chmod +x gradlew
