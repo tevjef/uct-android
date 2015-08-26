afa3fb618cbc0e1a2e06276932f6876404dc8f90
Tevin Jeffrey <mastermindtj94@gmail.com>
2015-08-26 17:40:56 -0400

    Updated the licenses for open source software.

---
+++ app/src/main/java/com/tevinjeffrey/rutgersct/ui/settings/License.java
@@ -0,0 +1,29 @@
+package com.tevinjeffrey.rutgersct.ui.settings;
+
+public class License {
+    String name;
+    String author;
+    String website;
+    String type;
+    String content;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
