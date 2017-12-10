ae66f57a8ea1a248148e73104e664a070a9b8f5e
Tevin Jeffrey <tev.jeffrey@gmail.com>
2017-12-10 11:30:33 -0500

    Handle notifications on Android 0

--- app/src/main/java/com/tevinjeffrey/rutgersct/data/notifications/UCTFirebaseMessagingService.kt
+++ app/src/main/java/com/tevinjeffrey/rutgersct/data/notifications/UCTFirebaseMessagingService.kt
@@ -1,11 +1,16 @@
 package com.tevinjeffrey.rutgersct.data.notifications
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
@@ -16,6 +21,7 @@ import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import com.tevinjeffrey.rutgersct.R
 import com.tevinjeffrey.rutgersct.data.model.UCTNotification
+import com.tevinjeffrey.rutgersct.ui.MainActivity
 import com.tevinjeffrey.rutgersct.utils.PreferenceUtils
 import dagger.android.AndroidInjection
 import timber.log.Timber
@@ -23,9 +29,12 @@ import java.io.IOException
 import java.math.BigInteger
 import javax.inject.Inject
 
+
 class UCTFirebaseMessagingService : FirebaseMessagingService() {
   @Inject lateinit var gson: Gson
   @Inject lateinit var mPreferenceUtils: PreferenceUtils
+
+  private lateinit var notificationManager: NotificationManager
 
   private val sound: Uri?
     get() = if (mPreferenceUtils.canPlaySound) {
@@ -34,9 +43,53 @@ class UCTFirebaseMessagingService : FirebaseMessagingService() {
       null
     }
 
+  @RequiresApi(Build.VERSION_CODES.N)
   override fun onCreate() {
     AndroidInjection.inject(this)
     super.onCreate()
+    notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+      // The id of the group.
+      val group = "section_group";
+      // The user-visible name of the group.
+      val groupName = "Sections"
+      notificationManager.createNotificationChannelGroup(NotificationChannelGroup(group, groupName))
+
+      val open: Unit = {
+        val id = "channel_section_open"
+        val name = "Open Sections"
+        val description = "Notify when sections open"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(id, name, importance)
+        channel.description = description
+        channel.enableLights(true)
+        channel.enableVibration(true)
+        channel.group = group
+        notificationManager.createNotificationChannel(channel)
+      }()
+
+      val closed: Unit = {
+        val id = "channel_section_closed"
+        val name = "Closed Sections"
+        val description = "Notify when sections closed"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(id, name, importance)
+        channel.description = description
+        channel.enableLights(true)
+        channel.enableVibration(true)
+        channel.group = group
+        notificationManager.createNotificationChannel(channel)
+      }()
+
+      val generic: Unit = {
+        val id = "channel_generic"
+        val name = "Other"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(id, name, importance)
+        notificationManager.createNotificationChannel(channel)
+      }()
+    }
   }
 
   override fun onMessageReceived(message: RemoteMessage) {
@@ -52,7 +105,25 @@ class UCTFirebaseMessagingService : FirebaseMessagingService() {
     sendNotification(data)
   }
 
-  private fun handleGenericNotification(message: RemoteMessage) {}
+  private fun handleGenericNotification(message: RemoteMessage) {
+    val notificationBuilder = NotificationCompat.Builder(this, "channel_generic")
+        .setSmallIcon(R.drawable.ic_notification)
+        .setWhen(System.currentTimeMillis())
+        .setContentTitle(message.notification.title)
+        .setContentText(message.notification.body)
+
+    val notificationIntent = Intent(this.applicationContext, MainActivity::class.java)
+
+    notificationIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+    val intent = PendingIntent.getActivity(applicationContext, 0, notificationIntent, 0)
+    notificationBuilder.setContentIntent(intent)
+
+    val notification = notificationBuilder.build()
+    notification.flags = notification.flags or Notification.FLAG_AUTO_CANCEL
+
+    message.notification
+    notificationManager.notify(0, notification)
+  }
 
   private fun sendNotification(message: String) {
     var uctNotification: UCTNotification? = null
@@ -67,29 +138,29 @@ class UCTFirebaseMessagingService : FirebaseMessagingService() {
     val course = subject.courses[0]
     val section = course.sections[0]
 
-    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-    val mBuilder: NotificationCompat.Builder
-
     val title: String
     val body: String
     val color: Int
+    val channel: String
+
     if (uctNotification.status == "Open") {
       title = "A section has opened!"
       body = "Section " + section.number + " of " + course.name + " has opened!"
       color = R.color.green
+      channel = "channel_section_open"
     } else {
       title = "A section has closed!"
       body = "Section " + section.number + " of " + course.name + " has closed!"
       color = R.color.red
+      channel = "channel_section_closed"
     }
 
-    mBuilder = NotificationCompat.Builder(this)
+    val notificationBuilder = NotificationCompat.Builder(this, channel)
         .setStyle(NotificationCompat.BigTextStyle()
             .bigText(body)
             .setBigContentTitle(course.name))
         .setSmallIcon(R.drawable.ic_notification)
         .setWhen(System.currentTimeMillis())
-        .setChannelId(SECTION_CHANNEL)
         .setPriority(NotificationCompat.PRIORITY_MAX)
         .setGroup(SECTION_NOTIFICATION_GROUP)
         .setColor(ContextCompat.getColor(this, color))
@@ -102,12 +173,12 @@ class UCTFirebaseMessagingService : FirebaseMessagingService() {
     val openInBrowser = Intent(Intent.ACTION_VIEW)
     openInBrowser.data = Uri.parse("https://sims.rutgers.edu/webreg/")
     val pOpenInBrowser = PendingIntent.getActivity(this, 0, openInBrowser, 0)
-    mBuilder.addAction(R.drawable.ic_open_in_browser, "Webreg", pOpenInBrowser)
-    mBuilder.setContentIntent(pOpenInBrowser)
+    notificationBuilder.addAction(R.drawable.ic_open_in_browser, "Webreg", pOpenInBrowser)
+    notificationBuilder.setContentIntent(pOpenInBrowser)
 
     notificationManager.notify(
         BigInteger(System.currentTimeMillis().toString()).toInt(),
-        mBuilder.build()
+        notificationBuilder.build()
     )
 
     Answers.getInstance()
@@ -121,5 +192,9 @@ class UCTFirebaseMessagingService : FirebaseMessagingService() {
   companion object {
     private val SECTION_NOTIFICATION_GROUP = "SECTION_NOTIFICATION_GROUP"
     private val SECTION_CHANNEL = "SECTION_CHANNEL"
+
+
+    private val sectionChannelName = "Section"
+    private val sectionChannelDescription = "Notifications for section status changes."
   }
 }
